3d1d55336c19babef2156750cec29fd7952a6e02
shunto <shunto.ykk1210@gmail.com>
2022-09-15 23:00:32 +0900

    :sparkles: quasar導入

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -1,91 +1,30 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
+  <q-layout view="hHh lpR fFf">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <q-header elevated class="bg-primary text-white" height-hint="98">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          書籍管理アプリ
+        </q-toolbar-title>
+      </q-toolbar>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+      <q-tabs align="left">
+        <q-route-tab to="/books" label="書籍一覧" />
+        <q-route-tab to="/new" label="新規追加" />
+        <!-- <q-route-tab to="/page3" label="Page Three" /> -->
+      </q-tabs>
+    </q-header>
 
-  <RouterView />
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
--- src/main/frontend/src/main.ts
+++ src/main/frontend/src/main.ts
@@ -1,11 +1,38 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import { Quasar } from 'quasar'
+import quasarLang from 'quasar/lang/ja'
+import quasarIconSet from 'quasar/icon-set/mdi-v6'
 import router from "./router";
-
 import "./assets/main.css";
 
-const app = createApp(App);
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/mdi-v6/mdi-v6.css'
 
-app.use(router);
+import 'quasar/src/css/index.sass'
 
-app.mount("#app");
+// Assumes your root component is App.vue
+// and placed in same folder as main.js
+import App from './App.vue'
+
+const app = createApp(App)
+
+app.use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
+  lang: quasarLang,
+  iconSet: quasarIconSet,
+  /*
+  config: {
+    brand: {
+      // primary: '#e46262',
+      // ... or all other brand colors
+    },
+    notify: {...}, // default set of options for Notify Quasar plugin
+    loading: {...}, // default set of options for Loading Quasar plugin
+    loadingBar: { ... }, // settings for LoadingBar Quasar plugin
+    // ..and many more (check Installation card on each Quasar component/directive/plugin)
+  }
+  */
+}).use(router)
+
+// Assumes you have a <div id="app"></div> in your index.html
+app.mount('#app')
