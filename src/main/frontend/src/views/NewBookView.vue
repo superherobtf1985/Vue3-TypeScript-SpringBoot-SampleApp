c7200e739bcbac83237673e31f345d18f9597087
shunto <shunto.ykk1210@gmail.com>
2022-09-23 22:29:33 +0900

    :recycle: コンポーネント整理

--- src/main/frontend/src/views/NewBookView.vue
+++ src/main/frontend/src/views/NewBookView.vue
@@ -1,7 +1,42 @@
-<script setup lang="ts">
-import Form from "../components/morecules/Form.vue";
-</script>
-
 <template>
-    <Form />
+    <Form @submit="onSubmit" />
 </template>
+
+<script setup lang="ts">
+import { useQuasar } from 'quasar'
+import { ref } from "vue";
+import { useRoute, useRouter } from 'vue-router'
+
+import BookApiService from "@/service/BookApiService";
+import Form from "../components/morecules/Form.vue";
+
+const $q = useQuasar()
+const router = useRouter()
+
+const title = ref(null)
+const author = ref(null)
+const detail = ref(null)
+
+type Book = {
+    title: string,
+    author: string,
+    detail: string
+}
+
+const onSubmit = (book: Book) => {
+  BookApiService.create(book).then(res => {
+    if (res.data) {
+      $q.notify({
+        type: 'positive',
+        message: '登録しました'
+      })
+      router.push('/books')
+    }
+  }).catch(err => {
+    $q.notify({
+      type: 'negative',
+      message: 'エラーが発生しました'
+    })
+  })
+}
+</script>
