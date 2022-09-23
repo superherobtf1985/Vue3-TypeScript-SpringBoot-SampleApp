e3496a4620157659c2ec2898fdde87cc8ce0b2f3
shunto <shunto.ykk1210@gmail.com>
2022-09-23 22:38:29 +0900

    storybook導入

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -1,30 +1,27 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
+import RouteTab from "./components/morecules/RouteTab.vue";
 </script>
 
 <template>
   <q-layout view="hHh lpR fFf">
-
     <q-header elevated class="bg-primary text-white" height-hint="98">
       <q-toolbar>
         <q-toolbar-title>
           <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
           </q-avatar>
           書籍管理アプリ
         </q-toolbar-title>
       </q-toolbar>
 
       <q-tabs align="left">
-        <q-route-tab to="/books" label="書籍一覧" />
-        <q-route-tab to="/new" label="新規追加" />
-        <!-- <q-route-tab to="/page3" label="Page Three" /> -->
+        <route-tab />
       </q-tabs>
     </q-header>
 
     <q-page-container>
       <router-view />
     </q-page-container>
-
   </q-layout>
 </template>
